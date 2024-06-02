cb8ff5bc95ff702b3f1d1cec811cf35ba5158b14
glycin <alex.chatzizacharias@gmail.com>
2024-06-03 01:58:59 +0200

    Added stateinvaders starter

---
+++ intelligame/intelligame/src/main/kotlin/com/glycin/intelligame/stateinvaders/StateInvadersService.kt
@@ -0,0 +1,13 @@
+package com.glycin.intelligame.stateinvaders
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+
+@Service
+class StateInvadersService {
+
+    fun initGame(project: Project, editor: Editor) {
+
+    }
+}
---
+++ intelligame/intelligame/src/main/kotlin/com/glycin/intelligame/stateinvaders/StateInvadersStarter.kt
@@ -0,0 +1,22 @@
+package com.glycin.intelligame.stateinvaders
+
+import com.intellij.codeInsight.intention.HighPriorityAction
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.openapi.components.service
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+
+class StateInvadersStarter: IntentionAction, HighPriorityAction {
+    override fun startInWriteAction(): Boolean = false
+
+    override fun getText(): String = "Time to abolish the state!"
+
+    override fun getFamilyName(): String = "IntelliGame"
+
+    override fun isAvailable(p0: Project, p1: Editor?, p2: PsiFile?): Boolean = true
+
+    override fun invoke(project: Project, editor: Editor, file: PsiFile) {
+        project.service<StateInvadersService>().initGame(project, editor)
+    }
+}
